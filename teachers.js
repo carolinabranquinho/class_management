d643b3bcf35b74af190deb29c785b95a0876ecad
Carolina Branquinho <carol.a.branquinho@gmail.com>
2020-07-23 18:16:11 -0300

    add post route

---
+++ teachers.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const data = require("./data.json");
+
+exports.post = function (req, res) {
+  let keys = Object.keys(req.body);
+
+  for (key of keys) {
+    if (req.body[key] == "") {
+      return res.send("Please, fill all fields!");
+    }
+  }
+
+  let { avatar_url, name, birth, scholarity, type_class, subjects } = req.body;
+
+  birth = Date.parse(req.body.birth);
+  const created_at = Date.now();
+  const id = Number(data.teachers.length + 1);
+
+  data.teachers.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    scholarity,
+    type_class,
+    subjects,
+    created_at,
+  });
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    return err ? res.send("Write file error!") : res.redirect("/teachers");
+  });
+};
