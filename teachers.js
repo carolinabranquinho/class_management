0dd2561c1be7d76b6cf58c56de348ace23f2138c
Carolina Branquinho <carol.a.branquinho@gmail.com>
2020-07-24 17:33:03 -0300

    add show teacher profile logic

--- teachers.js
+++ teachers.js
@@ -1,7 +1,25 @@
 const fs = require("fs");
 const data = require("./data.json");
+const { calc_age } = require("./utils");
 
-exports.show = function (req, res) {};
+exports.show = function (req, res) {
+  const { id } = req.params;
+
+  const foundTeacher = data.teachers.find((teacher) => {
+    return teacher.id == id;
+  });
+
+  const teacher = {
+    ...foundTeacher,
+    age: calc_age(foundTeacher.birth),
+    subjects: foundTeacher.subjects.split(","),
+    date: new Intl.DateTimeFormat("pt-BR").format(foundTeacher.created_at),
+  };
+
+  return !foundTeacher
+    ? res.send("Teacher doesn't exist!")
+    : res.render("teachers/show", { teacher });
+};
 
 exports.post = function (req, res) {
   let keys = Object.keys(req.body);
