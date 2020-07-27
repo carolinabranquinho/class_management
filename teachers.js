5619e06e2f03306cc0e9907e4c2ba9ece2b35d4a
Carolina Branquinho <carol.a.branquinho@gmail.com>
2020-07-27 11:56:58 -0300

    add put and delete routes

--- teachers.js
+++ teachers.js
@@ -68,3 +68,45 @@ exports.edit = function (req, res) {
 
   return res.render("teachers/edit", { teacher });
 };
+
+exports.put = function (req, res) {
+  const { id } = req.body;
+  let index = 0;
+
+  const foundTeacher = data.teachers.find((teacher, foundIndex) => {
+    if (teacher.id == id) {
+      index = foundIndex;
+      return true;
+    }
+  });
+
+  if (!foundTeacher) return res.send("Teacher doesn't exist!");
+
+  const teacher = {
+    ...foundTeacher,
+    ...req.body,
+    birth: Date.parse(req.body.birth),
+  };
+
+  data.teachers[index] = teacher;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    return err
+      ? res.send("Teacher not found!")
+      : res.redirect(`/teachers/${id}`);
+  });
+};
+
+exports.delete = function (req, res) {
+  const { id } = req.body;
+
+  const filteredTeachers = data.teachers.filter((teacher) => {
+    return teacher.id !== id;
+  });
+
+  data.teachers = filteredTeachers;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    return err ? res.send("Write File error") : res.redirect("/teachers");
+  });
+};
--- utils.js
+++ utils.js
@@ -15,9 +15,9 @@ module.exports = {
   calc_date: function (timestamp) {
     const date = new Date(timestamp);
 
-    const year = date.getFullYear();
-    const month = `0${date.getMonth()}`.slice(-2);
-    const day = `0${date.getDate()}`.slice(-2);
+    const year = date.getUTCFullYear();
+    const month = `0${date.getUTCMonth()}`.slice(-2);
+    const day = `0${date.getUTCDate()}`.slice(-2);
 
     return `${year}-${month}-${day}`;
   },
