2e3059313982ea5e0923a5d4035db06bbd658d18
Carolina Branquinho <carol.a.branquinho@gmail.com>
2020-08-12 10:39:56 -0300

    add teacher model

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -1,17 +1,17 @@
 const { calc_age, calc_date } = require("../../lib/utils");
+import Teacher from "../models/Teacher";
 
 module.exports = {
+  index(req, res) {
+    Teacher.all((teacher) => {
+      return res.render("teachers/index", { teacher });
+    });
+  },
+
   create(req, res) {
     return res.render("teachers/create");
   },
-  index(req, res) {
-    return res.render("teachers/index", { teachers: data.teachers });
-  },
-  show(req, res) {
-    const { id } = req.params;
 
-    return res.render("teachers/show", { teacher });
-  },
   post(req, res) {
     let keys = Object.keys(req.body);
 
@@ -21,21 +21,53 @@ module.exports = {
       }
     }
 
-    return res.redirect("/teachers");
+    Teacher.create(req.body, (teacher) => {
+      return res.redirect(`/teachers/${teacher.id}`);
+    });
   },
-  edit(req, res) {
+
+  show(req, res) {
     const { id } = req.params;
 
-    return res.render("teachers/edit", { teacher });
-  },
-  put(req, res) {
-    const { id } = req.body;
+    Teacher.find(id, (teacher) => {
+      if (!teacher) return res.send("Teacher not found");
 
-    return res.redirect(`/teachers/${id}`);
+      teacher.age = calc_age(teacher.birth_date);
+      teacher.subjects_taught = teacher.subjects_taught.split(",");
+      teacher.created_at = calc_date(teacher.created_at).format;
+
+      return res.render("teachers/show", { teacher });
+    });
   },
+
+  edit(req, res) {
+    const { id } = req.params;
+    Teacher.find(id, (teacher) => {
+      if (!teacher) return res.send("Teacher not found!");
+
+      teacher.birth_date = calc_date(teacher.birth_date).iso;
+
+      return res.render("teachers/edit", { teacher });
+    });
+  },
+
+  put(req, res) {
+    let keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") return res.send("Please, fill all fields!");
+    }
+
+    Teacher.update(req.body, () => {
+      return res.redirect(`/teachers/${req.body.id}`);
+    });
+  },
+
   delete(req, res) {
     const { id } = req.body;
 
-    return res.redirect("/teachers");
+    Teacher.delete(id, () => {
+      return res.redirect("/teachers");
+    });
   },
 };
---
+++ src/app/models/Teacher.js
@@ -0,0 +1,94 @@
+const db = require("../../config/db");
+const { calc_date } = require("../../lib/utils");
+
+module.exports = {
+  all(callback) {
+    db.query(`SELECT * FROM teachers ORDER BY name ASC`, function (
+      err,
+      results
+    ) {
+      if (err) throw `Database error! ${err}`;
+      callback(results.rows);
+    });
+  },
+
+  create(data, callback) {
+    const query = `
+    INSERT INTO teachers(
+      name, 
+      avatar_url,
+      birth_date,
+      education_level,
+      class_type,
+      subjects_taught,
+      created_at
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    RETURNING id
+  `;
+
+    const values = [
+      data.name,
+      data.avatar_url,
+      calc_date(data.birth_date).iso,
+      data.education_level,
+      data.class_type,
+      data.subjects_taught,
+      calc_date(Date.now()).iso,
+    ];
+
+    db.query(query, values, function (err, results) {
+      if (err) throw `Database create error! ${err}`;
+      console.log(results.rows[0]);
+      callback(results.rows[0]);
+    });
+  },
+
+  find(id, callback) {
+    db.query(`SELECT * FROM teachers WHERE id = $1`, [id], function (
+      err,
+      results
+    ) {
+      if (err) throw `Database find error! ${err}`;
+      callback(results.rows[0]);
+    });
+  },
+
+  update(data, callback) {
+    const query = `
+      UPDATE teachers SET
+        avatar_url=($1),
+        name=($2),
+        birth_date=($3),
+        education_level=($4),
+        class_type=($5),
+        subjects_taught=($6),
+      WHERE id = $7
+    `;
+
+    const values = [
+      data.avatar_url,
+      data.name,
+      calc_date(data.birth_date).iso,
+      data.education_level,
+      data.class_type,
+      data.subjects_taught,
+      data.id,
+    ];
+
+    db.query(query, values, function (err, results) {
+      if (err) throw `Database update error! ${err}`;
+      callback();
+    });
+  },
+
+  delete(id, callback) {
+    db.query(`DELETE FROM teachers WHERE id = $1`, [id], function (
+      err,
+      results
+    ) {
+      if (err) throw `Database delete Error! ${err}`;
+
+      return callback();
+    });
+  },
+};
